8ec9f623e9bd06e2dfd44d5e337eab3568eb4114
Jayson Concha <jmconcha.pro@gmail.com>
2023-02-22 20:45:34 +0800

    base.html and front_page.html templates

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
