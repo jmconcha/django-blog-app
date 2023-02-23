cc825b910cec85774b53aa0abfaef40b8e0bd5ef
Jayson Concha <jmconcha.pro@gmail.com>
2023-02-23 14:57:12 +0800

    Comment model created

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,12 @@ class Blog(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, related_name='rel_comment', on_delete=models.CASCADE)
+    slug = models.SlugField()
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.comment_text[:100]
