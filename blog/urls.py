2f4735acc7f67418d7a26e8bfac2657e319223c0
Jayson Concha <jmconcha.pro@gmail.com>
2023-02-23 20:08:45 +0800

    comment on a blog functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('blog/create/', views.create_blog, name='create_blog'),
     path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
+    path('blog/<slug:blog_slug>/comment/', views.comment_on_blog, name='comment_on_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,29 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 from .models import Blog
-from .forms import BlogForm
+from .forms import BlogForm, CommentForm
 
 # Create your views here.
 def blog_detail(request, slug):
     blog = get_object_or_404(Blog, slug=slug)
     
+    comment_form = CommentForm()
+    comment_form.fields['comment_text'].widget.attrs['class'] = 'textarea'
+    comment_form.fields['comment_text'].widget.attrs['rows'] = '3'
+    comment_form.fields['comment_text'].widget.attrs['placeholder'] = 'Write a comment...'
+    
+    if comment_form['comment_text'].errors:
+        comment_form.fields['comment_text'].widget.attrs['class'] += ' is-danger'
+    
     return render(request, 'blog/blog_detail.html', {
         'blog': blog,
+        'comment_form': comment_form
     })
     
 def create_blog(request):
     if request.method == 'POST':
-        form = BlogForm(request.POST)
+        form = BlogForm(request['POST'])
         if form.is_valid():
             form.save()
             
@@ -27,4 +37,20 @@ def create_blog(request):
     
     return render(request, 'blog/create_blog.html', {
         'form': form,
+    })
+    
+def comment_on_blog(request, blog_slug):
+    if request.method == 'POST':
+        blog = get_object_or_404(Blog, slug=blog_slug)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.blog = blog
+            comment.save()
+            
+            return redirect(reverse('blog:blog_detail', args=(blog_slug,)))
+    
+    return render(request, 'blog/blog_detail', {
+        'blog': blog,
+        'comment_form': CommentForm(),
     })
