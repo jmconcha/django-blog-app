b256c1eedf7c589801fe50832d7530f1b917b6fc
Jayson Concha <jmconcha.pro@gmail.com>
2023-02-24 20:09:47 +0800

    create slugify and generate_slug function

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
+from .utils import remove_non_alphanumeric
+
 # Create your tests here.
+
+class UtilsTests(TestCase):
+    def test_remove_non_alphanumeric(self):
+        input_string = "Hello python abs"
+        expected_output = "Hello python abs"
+        self.assertEqual(remove_non_alphanumeric(input_string), expected_output)
+        
+        input_string = "Hello, $!%world123"
+        expected_output = "Hello world123"
+        self.assertEqual(remove_non_alphanumeric(input_string), expected_output)
+
+        input_string = "abc!@#123"
+        expected_output = "abc123"
+        self.assertEqual(remove_non_alphanumeric(input_string), expected_output)
+
+        input_string = "python3.9.1"
+        expected_output = "python391"
+        self.assertEqual(remove_non_alphanumeric(input_string), expected_output)
+
+        input_string = "a b c"
+        expected_output = "a b c"
+        self.assertEqual(remove_non_alphanumeric(input_string), expected_output)
---
+++ blog/utils.py
@@ -0,0 +1,30 @@
+import string
+import random
+import re
+
+def generate_slug(count=10, letters=string.ascii_letters, digits=string.digits):
+    alphanumeric = letters + digits
+    result = ''
+    
+    for c in range(count):
+        result += random.choice(alphanumeric)
+        if c % 5 == 0:
+            result += random.choice('_-')
+        
+    return result
+
+def remove_non_alphanumeric(input_string):
+    # Define a regular expression pattern to match all non-alphanumeric characters
+    pattern = r'[^\sa-zA-Z0-9]'
+
+    # Use re.sub to replace all non-alphanumeric characters with an empty string
+    output_string = re.sub(pattern, '', input_string)
+
+    return output_string
+
+
+def slugify(chars):
+    alphanumeric = remove_non_alphanumeric(chars)
+    slug = re.sub(r'\s', '-', alphanumeric)
+    
+    return slug
