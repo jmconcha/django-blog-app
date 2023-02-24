62ecdcbcfb2aa73e20665b9b69c0bdb8f2487963
Jayson Concha <jmconcha.pro@gmail.com>
2023-02-24 13:58:38 +0800

    done custom login and logout functionality

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'core'
 urlpatterns = [
-    path('', views.front_page, name='front_page')
+    path('', views.front_page, name='front_page'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 from blog.models import Blog
 
@@ -10,3 +12,23 @@ def front_page(request):
     return render(request, 'core/front_page.html', {
         'blogs': blogs,
     })
+    
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user == None:
+            messages.add_message(request, messages.WARNING, "Your username and password didn't match. Please try again.")
+        else:
+            login(request, user)
+            return redirect(reverse('core:front_page'))
+        
+    return render(request, 'core/login.html')
+
+def logout_user(request):
+    if request.user.is_authenticated:
+        logout(request)
+    
+    return redirect(reverse('core:front_page'))
