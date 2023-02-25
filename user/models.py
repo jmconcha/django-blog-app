0d9aacf8821f22e8c3f05cd6299a30aed65cf5d4
Jayson Concha <jmconcha.pro@gmail.com>
2023-02-25 14:37:41 +0800

    create UserProfile model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='rel_profile', on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(blank=True, null=True)
