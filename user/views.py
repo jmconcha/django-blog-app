6e50bb021f69aa44b03ff3996abd513c161bbc60
Jayson Concha <jmconcha.pro@gmail.com>
2023-03-11 17:25:14 +0800

    user_profile view and templates

--- user/views.py
+++ user/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.exceptions import PermissionDenied
 
 from blog.models import Blog
+from .models import UserProfile
 
 # Create your views here.
 def user_blogs(request, username):
@@ -26,4 +30,29 @@ def user_blogs(request, username):
               'DRAFT': Blog.DRAFT,
               'ACTIVE': Blog.ACTIVE,
             },
+        })
+
+@login_required(login_url=reverse_lazy('core:login'))
+def user_profile(request, username):
+    if request.user.username != username:
+        raise PermissionDenied
+    
+    user_profile = request.user.rel_profile
+    user_info = {
+        'username': request.user.username,
+        'firstname': request.user.first_name,
+        'lastname': request.user.last_name,
+        'email': request.user.email,
+        'bio': user_profile.bio,
+        'location': user_profile.location,
+        'birth_date': user_profile.birth_date,
+    }
+    
+    print(user_info)
+    
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'user/profile.html', {
+            'user_info': user_info,
         })
