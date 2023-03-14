6fef536283f3030a4b104147bc7b51b1283c981a
Jayson Concha <jmconcha.pro@gmail.com>
2023-03-14 13:21:35 +0800

    exclude blog slug field in Admin site, then auto populate it using title_as_slug util function

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
 from .models import Blog, Comment
+from .utils import title_as_slug
+
+
+class BlogAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+    def save_model(self, request, obj, form, change):
+        obj.slug = title_as_slug(obj.title)
+        super().save_model(request, obj, form, change)
+
 
 # Register your models here.
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
 admin.site.register(Comment)
