f7ff8e268fdfece1394e681a9a24899ab6df9e19
Jayson Concha <jmconcha.pro@gmail.com>
2023-03-14 21:40:58 +0800

    add user field in Comment model

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
 class Blog(models.Model):
     DRAFT = 'draft'
     ACTIVE = 'active'
@@ -10,22 +12,27 @@ class Blog(models.Model):
         (DRAFT, 'Draft'),
         (ACTIVE, 'Active'),
     )
-    
+
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField()
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=10, choices=CHOICE_STATUS, default=DRAFT)
-    
+    status = models.CharField(
+        max_length=10, choices=CHOICE_STATUS, default=DRAFT)
+
     def __str__(self):
         return self.title
-    
+
+
 class Comment(models.Model):
-    blog = models.ForeignKey(Blog, related_name='rel_comments', on_delete=models.CASCADE)
+    blog = models.ForeignKey(
+        Blog, related_name='rel_comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name='rel_comments', on_delete=models.CASCADE)
     slug = models.SlugField()
     comment_text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.comment_text[:100]
