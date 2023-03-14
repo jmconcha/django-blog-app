fc3be33f329410a719c6b364b8b4dfaf607b2618
Jayson Concha <jmconcha.pro@gmail.com>
2023-03-14 19:18:37 +0800

    add list_display, list_filter, search_fields for Blog model in Django admin site

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,10 @@ from .utils import title_as_slug
 
 class BlogAdmin(admin.ModelAdmin):
     exclude = ('slug',)
+    list_filter = ('status',)
+    list_display = ('author', 'title', 'body', 'created_at')
+    search_fields = ('title__icontains',
+                     'body__icontains', 'author__username__iexact', 'author__first_name__icontains', 'author__last_name__icontains')
 
     def save_model(self, request, obj, form, change):
         obj.slug = title_as_slug(obj.title)
